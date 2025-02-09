9c9875470e1c4a2a68478f00aca099dad0c407e1
Shaifali07 <56667719+Shaifali07@users.noreply.github.com>
2025-02-09 09:50:28 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_zHcB8cadP', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2uk9idd66lb3s01tguv9gebdb4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4533egs9q78f3tiosacfdpi2hu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
